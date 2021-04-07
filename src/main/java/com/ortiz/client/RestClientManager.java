a39add5167673a659948ffa0fb1654e1f4441963
jtortiz27 <38021345+jtortiz27@users.noreply.github.com>
2021-04-07 13:01:02 -0500

    Updated to include POST, PUT, PATCH and DELETE methods to RestClientManager. Updated Javadoc accordingly

--- src/main/java/com/ortiz/client/RestClientManager.java
+++ src/main/java/com/ortiz/client/RestClientManager.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import com.ortiz.model.RestApiResult;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.SignalType;
 import reactor.netty.http.client.HttpClient;
@@ -33,7 +35,7 @@ public class RestClientManager {
      * @param returnType Type to deserialize to
      * @return APIResult with deserialized value of response
      */
-    public <T> Mono<RestApiResult<T>> getResourceAsync(String url, final Class<T> returnType) {
+    public <T> Mono<RestApiResult<T>> getResourceAsync(String url, Class<T> returnType) {
         RestApiResult<T> restApiResult = new RestApiResult<>();
         try {
             return httpClient.get()
@@ -84,7 +86,7 @@ public class RestClientManager {
      * @param returnType Type to deserialize list to
      * @return APIResult with deserialized value of responses
      */
-    public <T> Mono<RestApiResult<T>> getResourcesAsync(String url, final Class<T> returnType) {
+    public <T> Mono<RestApiResult<T>> getResourcesAsync(String url, Class<T> returnType) {
         RestApiResult<T> restApiResult = new RestApiResult<>();
         try {
             return httpClient.get()
@@ -127,9 +129,214 @@ public class RestClientManager {
     }
 
     /**
+     * This Method should be used to make an HTTP POST call and shall deserialize the result to whatever class passed in <T>
+     * <p>
+     * Note: This is for retrieving a list of resources and upon success, the deserialized result will be available in APIResult.getSuccessResult()
+     *
+     * @param url          url of resources to retrieve
+     * @param objectToPost Type to deserialize list to
+     * @return APIResult with deserialized value of responses
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Mono<RestApiResult<T>> postResourceAsync(String url, T objectToPost) {
+        RestApiResult<T> restApiResult = new RestApiResult<>();
+        try {
+            String postData = mapper.writeValueAsString(objectToPost);
+            ByteBuf postBodyByteBuf = Unpooled.copiedBuffer(postData.getBytes());
+            httpClient.post()
+                    .uri(url)
+                    .send(Mono.just(postBodyByteBuf))
+                    .responseSingle((httpClientResponse, byteBufMono) -> {
+                        //Populate Request metadata
+                        restApiResult.setRequestPath(httpClientResponse.fullPath());
+                        restApiResult.setHeaders(httpClientResponse.requestHeaders());
+                        restApiResult.setHttpMethod(httpClientResponse.method());
+                        restApiResult.setClientResponse(httpClientResponse);
+
+                        //Determine if success
+                        int responseStatus = httpClientResponse.status().code();
+
+                        //If success, emit response as string to reactive flow
+                        if (responseStatus >= 200 && responseStatus < 300) {
+                            restApiResult.setSuccess(true);
+                            return byteBufMono.asString();
+                        }
+
+                        //If error status code, throw error
+                        return Mono.error(new Exception("Received Error Status Code"));
+                    })
+                    .flatMap(s -> {
+                        try {
+                            //Attempt to deserialize and return ApiResult
+                            restApiResult.setSuccessResult(deserialize(s.getBytes(StandardCharsets.UTF_8), (Class<T>) (objectToPost.getClass())));
+                            return Mono.just(restApiResult);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return Mono.error(e);
+                        }
+                    }).single();
+            return Mono.just(restApiResult);
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+
+    /**
+     * This Method should be used to make an HTTP PATCH call and shall deserialize the result to whatever class passed in <T>
+     * <p>
+     * Note: This is for patching a resource and upon success, the deserialized result will be available in APIResult.getSuccessResult()
+     *
+     * @param url           url of resources to patch
+     * @param objectToPatch Type to deserialize list to
+     * @return APIResult with deserialized value of patched resource
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Mono<RestApiResult<T>> patchResourceAsync(String url, T objectToPatch) {
+        RestApiResult<T> restApiResult = new RestApiResult<>();
+        try {
+            String postData = mapper.writeValueAsString(objectToPatch);
+            ByteBuf postBodyByteBuf = Unpooled.copiedBuffer(postData.getBytes());
+            httpClient.patch()
+                    .uri(url)
+                    .send(Mono.just(postBodyByteBuf))
+                    .responseSingle((httpClientResponse, byteBufMono) -> {
+                        //Populate Request metadata
+                        restApiResult.setRequestPath(httpClientResponse.fullPath());
+                        restApiResult.setHeaders(httpClientResponse.requestHeaders());
+                        restApiResult.setHttpMethod(httpClientResponse.method());
+                        restApiResult.setClientResponse(httpClientResponse);
+
+                        //Determine if success
+                        int responseStatus = httpClientResponse.status().code();
+
+                        //If success, emit response as string to reactive flow
+                        if (responseStatus >= 200 && responseStatus < 300) {
+                            restApiResult.setSuccess(true);
+                            return byteBufMono.asString();
+                        }
+
+                        //If error status code, throw error
+                        return Mono.error(new Exception("Received Error Status Code"));
+                    })
+                    .flatMap(s -> {
+                        try {
+                            //Attempt to deserialize and return ApiResult
+                            restApiResult.setSuccessResult(deserialize(s.getBytes(StandardCharsets.UTF_8), (Class<T>) (objectToPatch.getClass())));
+                            return Mono.just(restApiResult);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return Mono.error(e);
+                        }
+                    }).single();
+            return Mono.just(restApiResult);
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+
+    /**
+     * This Method should be used to make an HTTP PUT call and shall deserialize the result to whatever class passed in <T>
+     *
+     * @param url         url of resources to PUT
+     * @param objectToPut Type to deserialize list to
+     * @return APIResult with deserialized value of response
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Mono<RestApiResult<T>> putResourceAsync(String url, T objectToPut) {
+        RestApiResult<T> restApiResult = new RestApiResult<>();
+        try {
+            String postData = mapper.writeValueAsString(objectToPut);
+            ByteBuf postBodyByteBuf = Unpooled.copiedBuffer(postData.getBytes());
+            httpClient.put()
+                    .uri(url)
+                    .send(Mono.just(postBodyByteBuf))
+                    .responseSingle((httpClientResponse, byteBufMono) -> {
+                        //Populate Request metadata
+                        restApiResult.setRequestPath(httpClientResponse.fullPath());
+                        restApiResult.setHeaders(httpClientResponse.requestHeaders());
+                        restApiResult.setHttpMethod(httpClientResponse.method());
+                        restApiResult.setClientResponse(httpClientResponse);
+
+                        //Determine if success
+                        int responseStatus = httpClientResponse.status().code();
+
+                        //If success, emit response as string to reactive flow
+                        if (responseStatus >= 200 && responseStatus < 300) {
+                            restApiResult.setSuccess(true);
+                            return byteBufMono.asString();
+                        }
+
+                        //If error status code, throw error
+                        return Mono.error(new Exception("Received Error Status Code"));
+                    })
+                    .flatMap(s -> {
+                        try {
+                            //Attempt to deserialize and return ApiResult
+                            restApiResult.setSuccessResult(deserialize(s.getBytes(StandardCharsets.UTF_8), (Class<T>) (objectToPut.getClass())));
+                            return Mono.just(restApiResult);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return Mono.error(e);
+                        }
+                    }).single();
+            return Mono.just(restApiResult);
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+
+    /**
+     * This Method should be used to make an HTTP DELETE call and shall return an empty successResult <T>
+     * <p>
+     *
+     * @param url url of resources to delete
+     * @return APIResult with empty successResult
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Mono<RestApiResult<T>> deleteResourceAsync(String url) {
+        RestApiResult<T> restApiResult = new RestApiResult<>();
+        try {
+            httpClient.delete()
+                    .uri(url)
+                    .responseSingle((httpClientResponse, byteBufMono) -> {
+                        //Populate Request metadata
+                        restApiResult.setRequestPath(httpClientResponse.fullPath());
+                        restApiResult.setHeaders(httpClientResponse.requestHeaders());
+                        restApiResult.setHttpMethod(httpClientResponse.method());
+                        restApiResult.setClientResponse(httpClientResponse);
+
+                        //Determine if success
+                        int responseStatus = httpClientResponse.status().code();
+
+                        //If success, emit response as string to reactive flow
+                        if (responseStatus == 200 || responseStatus == 204) {
+                            restApiResult.setSuccess(true);
+                            return byteBufMono.asString();
+                        }
+
+                        //If error status code, throw error
+                        return Mono.error(new Exception("Received Error Status Code"));
+                    })
+                    .flatMap(s -> {
+                        try {
+                            //Attempt to deserialize and return ApiResult
+                            restApiResult.setSuccessResult(null);
+                            return Mono.just(restApiResult);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            return Mono.error(e);
+                        }
+                    }).single();
+            return Mono.just(restApiResult);
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+
+    /**
      * This method allows for the asynchronous deserialization of a byte[] to whatever Type provided
      *
-     * @param jsonBytes byte array to feed into async parser
+     * @param jsonBytes  byte array to feed into async parser
      * @param returnType type to deserialize
      * @return deserialized value
      * @throws Exception throws exception if deserialization fails
